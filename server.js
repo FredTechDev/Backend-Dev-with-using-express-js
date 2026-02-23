be178aa9a385a4db27c57d34ad30e17536e14f2c
FredTechDev <Onyangofred1005@gmail.com>
2026-02-23 15:25:02 +0300

    changes made, .env file removed

--- server.js
+++ server.js
@@ -14,30 +14,52 @@ app.get('/',(req,res)=>{
 app.post('/users',(req,res)=>{
     res.json([{
         "First_name" : "Fredrick",
-        "Last_name" : "Onyango" 
+        "Last_name" : "Onyango" ,
+        "Id_Number" : "368736110",
+        "Course" : "Information Technology",
+        "Skills" : "Backend dev, Data Science and Ml",
+        "Phon_number" : "0113303477"
     },
     {
         "First_name" : "Michael",
-        "Last_name" : "Kimeli"
+        "Last_name" : "Kimeli",
+        "Id_number" : "09328946783",
+        "Course" : "Computer Science",
+        "Skills" : "Data analysis",
+        "Phone_number" : "0734343452"
     } ,
 
     {
         "First_name" : "Hills",
-        "Last_name" : "Erastus"
+        "Last_name" : "Erastus",
+        "Id_number" : "898667",
+        "Course" : "Forensics",
+        "Skills" : "Cyber security",
+        "Phone_number" : "080672656"
     } ,
 
     {
         "First_name" : "James",
-        "Last_name" : "Migosi"
+        "Last_name" : "Migosi",
+        "Id_number" : "786475734",
+        "Course" : "Software_Engineering",
+        "Skills" : "Mobile App development",
+        "Phone_number" : "0113464654"
     } ,
 
     {
         "First_name" : "Idah",
-        "Last_name" : "Miguel"
+        "Last_name" : "Miguel",
+        "Id_number" : "6788755",
+        "Course" : "Pharmaceuticals",
+        "Skills" : "Machine learning",
+        "Phone_number" : "0712992710"
     }])
 })
 
-
+app.get('/Freddie', (req, res)=>{
+    res.send("Fredy is happy codding his first server!")
+})
 
 
 
