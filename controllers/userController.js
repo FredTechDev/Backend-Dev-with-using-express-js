276c679f1c18d3c6422b348d1f1181b72b6f4d27
FredTechDev <onyangofred1005@gmail.com>
2026-03-25 12:55:17 +0300

    new updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,4 +90,19 @@ const userById = async (req, res) => {
   }
 };
 
-module.exports = {register, login, userById}
+const deleteuser = async (req, res) => {
+  try {
+    const user = await user.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = {register, login, userById, deleteuser}
